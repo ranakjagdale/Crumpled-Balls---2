03ebeee7f34e54a179fff546363e9098c5404770
ranakjagdale <65115164+ranakjagdale@users.noreply.github.com>
2020-06-02 16:57:39 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y, r){
+      var  options= {
+          isStatic: false,
+          restitution : 0.3,
+          friction : 0.5,
+          density : 1.2
+      }
+      
+      //this.image = loadImage("paper.png");
+      this.body = Bodies.circle(x, y, r,options);  
+      this.image = loadImage("paper.png");   
+      World.add(world,this.body);
+    }
+
+    display(){
+       var pos = this.body.position;  
+      // fill("yellow");  
+       ellipseMode(RADIUS);
+       ellipse(pos.x, pos.y, 35,35);
+       imageMode(CENTER);
+       image(this.image, pos.x, pos.y, 70, 70);
+    }
+}
